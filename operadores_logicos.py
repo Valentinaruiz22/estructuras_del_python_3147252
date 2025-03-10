9b1ec07f84b7c56385e9425e08e6393f6588b0a5
Camilo <camiloleon@gmail.com>
2025-03-10 11:17:35 -0500

    ejercicios de practica

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -44,3 +44,14 @@ print(" El resultado de operar con jerarquia de operadores es" ,y)
 
 #Ejemplo 5: operadores relacionales y logicos
 y= not 3 > 4 and 4 == 4 or 3 < 2
+
+#ejemplo 6: operadores aritmeticos,
+#relacionales y logicos
+y= 3 + 5 * 2 > 3 and 4 == 4 or 3 < 2
+
+
+#Ejemplo 7: con parentesis
+y= (3 + 5)* (2 > 3) and 4 == 4 or not 3 < 2
+
+#Ejemplo 8: todo junto
+y= 4**2 *3 < 6 /(7 - 5) and 7 * 2 +7 * 2 + 1 == 14 or not 3 + 5 < 2
