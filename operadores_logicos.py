e97320aff4bb6762099ac7865ab3acd5ae2f3b3d
Camilo <camiloleon@gmail.com>
2025-03-10 10:24:31 -0500

    jerarquia

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -19,3 +19,26 @@ print("El resultado de operar con and es" ,y)
 #ejemplo 3: Operador or
 y = False or False
 print(" El resultado de operar con or es" ,y)
+
+'''
+Jerarquia de predecia de operadores
+(logicos inclusive)
+1.    ()
+2.    **
+3.   *, /, %,
+4.   +, -
+5.   >,<,>=, <=, !=, ==
+6.    not 
+7.    and
+8.    or
+9.=
+
+'''
+
+#Ejemplo 4: Jerarquia de operadores
+y = False and not True or False
+print(" El resultado de operar con jerarquia de operadores es" ,y)
+
+
+
+
